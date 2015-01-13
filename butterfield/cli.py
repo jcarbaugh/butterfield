0a2ad1a564b7c6d08094a9d45fff5ab2c42b2f52
Paul Tagliamonte <tag@pault.ag>
2015-01-12 22:15:15 -0500

    Update test logging endpoint

--- butterfield/cli.py
+++ butterfield/cli.py
@@ -1,11 +1,15 @@
 import sys
+import json
 import asyncio
 from . import Bot, ALL
 
 
 @asyncio.coroutine
 def log(bot, message):
-    print(bot, message)
+    yield from bot.post(
+        'C035687FU', # Sunlight's #testing
+        "```{}```".format(json.dumps(message, sort_keys=True, indent=2))
+    )
 
 
 def main():
