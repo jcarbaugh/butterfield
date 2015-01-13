3697e00428af33d9691614fb271762dd9b7b99ae
Paul Tagliamonte <tag@pault.ag>
2015-01-12 21:45:59 -0500

    Reflow spacing

--- butterfield/handlers.py
+++ butterfield/handlers.py
@@ -3,5 +3,5 @@ import asyncio
 
 @asyncio.coroutine
 def joined_channel(bot, message):
-	channel = message['channel']
-	bot.channels[channel['id']] = channel
+    channel = message['channel']
+    bot.channels[channel['id']] = channel
