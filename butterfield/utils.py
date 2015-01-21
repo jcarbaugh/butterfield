766d714fafee072e263474e91830f3354c90589b
Jeremy Carbaugh <jcarbaugh@gmail.com>
2015-01-21 14:39:35 -0500

    add at_bot decorator to only handle message if it is directed at the bot

--- butterfield/utils.py
+++ butterfield/utils.py
@@ -1,4 +1,5 @@
 import importlib
+import re
 
 
 def load_plugin(name):
@@ -6,3 +7,12 @@ def load_plugin(name):
     module = importlib.import_module(module)
     coro = getattr(module, coroutine)
     return coro
+
+
+def at_bot(func):
+    def inner(bot, message: "message"):
+        match = re.match(r"<@{}>(?::)? (.*)".format(bot.id), message.get('text'))
+        if match:
+            message['text'] = match.group(1)
+            yield from func(bot, message)
+    return inner
