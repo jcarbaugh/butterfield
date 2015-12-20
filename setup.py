fd889bd4a88ed23126c720ed51fc5477ab0ae2a4
Alex.F <gfreezy@gmail.com>
2015-12-21 00:59:42 +0800

    add aiohttp dep

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     install_requires = [
         "websockets==2.3",
         "slacker-asyncio==0.8.6",
+        "aiohttp==0.19.0",
     ],
     entry_points = {'console_scripts': [
         'butterfield = butterfield.cli:main'
